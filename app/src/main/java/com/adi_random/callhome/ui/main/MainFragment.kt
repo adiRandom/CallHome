cf14d56485140e502085204bfc4b0e990db92b53
Adrian Pascu <me.adi.hf@gmail.com>
2020-08-17 16:19:25 +0300

    Initial commit

---
+++ app/src/main/java/com/adi_random/callhome/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.adi_random.callhome.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.adi_random.callhome.R
+import com.example.callhome.ui.main.MainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels<MainViewModel>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
